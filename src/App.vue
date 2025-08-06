a08793fb165ae6c4b5d7c20d78cb1cef3e565957
rommendurand77 <rommendurand77@gmail.com>
2025-08-06 01:55:29 +0000

    diseño de estruct inicial

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,52 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
+  import { ref } from "vue";
+  import { invoke } from "@tauri-apps/api/core";
+  import workSheetPanel from "./components/workSheet/workSheetMain.vue" 
 
-const greetMsg = ref("");
-const name = ref("");
+  const greetMsg = ref("");
+  const name = ref("");
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  console.log('Nombre'+name.value);
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
+  async function greet() {
+    console.log('Nombre'+name.value);
+    greetMsg.value = await invoke("greet", { name: name.value });
+  }
 </script>
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <!--Contenedor principal de los paneles-->
+    <section class="row">
+      <!-- Primer columna de Explorador de Data y Componentes-->
+      <aside class="column">
+        <p>Columna 1</p>
+        <form  @submit.prevent="greet">
+          <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+          <button type="submit">Greet</button>
+        </form>
+        <p>{{ greetMsg }}</p>
+      </aside> 
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img  class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img  class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+      <!-- Columna para la hoja de Trabajo-->
+      <aside id="workSheetPanel" class="column">
+        <workSheetPanel/>
+      </aside>
 
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+      <!-- Columna para las propiedades y config de un Componente-->
+      <aside class="column">
+        <p>"<<<<"Columna 3</p>
+      </aside>
+
+    </section> 
   </main>
 </template>
 
 <style scoped>
-/* SCOPED Estilo que solo afecta este componente, no es en cascada*/
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #249b73);
+  }
 
 </style>
---
+++ src/components/workSheet/workSheetMain.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const mainWs = ref('Hoja de Trabajo de Rommen');
+
+</script>
+
+<template>
+  <div>
+    <p>{{ mainWs }}</p>
+  </div>
+</template>
+
+<style scoped>
+/* Estilos locales para la hoja de trabajo*/
+</style>
